f858375a0b3099637843b35bbbb3d242d3c9646c
chochimus <127354521+chochimus@users.noreply.github.com>
2025-09-26 16:12:05 -0500

    Completed exercise

--- easy3/p10.py
+++ easy3/p10.py
@@ -1,32 +1,43 @@
 """
 Problem:
-Write a function that takes a string, doubles every character in the string, then returns the result as a new string.
+Write a function that takes a string, doubles every consonant in the string, 
+and returns the result as a new string. The function should not double vowels 
+('a','e','i','o','u'), digits, punctuation, or whitespace.
+
+You may assume that only ASCII characters will be included in the argument.
 
 input: string
-output: string with every character doubled
+output: string with every consonant doubled
 
 rules:
     explicit:
-    - all characters count
+    - only ascii characters included in the argument
+    - only consonants are doubled
     implicit:
-    - empty string should return empty string
+    - empty string returns empty
+    - all non-consonant chars are singled
 
 datastructure:
-text sequence
+text-sequence
 
 algorithm:
-1.) create empty string answer
-2.) add 2 of current char to answer
-3.) repeat for every character
-
+1.) create empty answer string
+2.) if char is a consonant append it twice, else append it once
+3.) repeat for all characters in input string
+4.) return string
 """
 
-def repeater(string):
+def double_consonants(string):
     answer = ""
     for char in string:
-        answer += char * 2
+        if char.isalpha() and char not in 'aeiouAEIOU':
+            answer += char * 2
+        else:
+            answer += char
     return answer
 
-print(repeater('Hello') == "HHeelllloo")              # True
-print(repeater('Good job!') == "GGoooodd  jjoobb!!")  # True
-print(repeater('') == "")                             # True
+# All of these examples should print True
+print(double_consonants('String') == "SSttrrinngg")
+print(double_consonants('Hello-World!') == "HHellllo-WWorrlldd!")
+print(double_consonants('July 4th') == "JJullyy 4tthh")
+print(double_consonants('') == "")
