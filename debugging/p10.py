b2534aa3673455ea1294486daf3ed3e8c277d6a7
chochimus <127354521+chochimus@users.noreply.github.com>
2025-10-09 17:24:23 -0500

    Updated solution

--- debugging/p10.py
+++ debugging/p10.py
@@ -2,8 +2,6 @@ data = [4, 2, 4, 2, 1, 3, 2, 3, 2, 4, 3]
 unique_data = list(set(data))
 print(unique_data == [4, 2, 1, 3]) # order not guaranteed
 
-# we could use a set comprehension
-
 unique_data = []
 seen = set()
 for num in data:
