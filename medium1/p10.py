8ce32c06e0a7697c6a12b6934ffa74eb0afd25df
chochimus <127354521+chochimus@users.noreply.github.com>
2025-10-14 16:06:59 -0500

    Completed exercise 10

---
+++ medium1/p10.py
@@ -0,0 +1,53 @@
+"""
+Problem:
+As we've seen in the last few exercises, the Fibonacci series is a computationally 
+simple series. However, the results grow at an incredibly rapid rate. 
+For example, the 100th Fibonacci number is 354,224,848,179,261,915,075 -- 
+that's enormous, especially considering that it takes six iterations just to 
+find the first 2-digit Fibonacci number.
+
+Write a function that calculates and returns the index of the first Fibonacci 
+number that has the number of digits specified by the argument. 
+The first Fibonacci number has an index of 1. You may assume that the 
+argument is always an integer greater than or equal to 2.
+
+
+"""
+
+
+import sys
+
+sys.set_int_max_str_digits(50_000)
+
+seen = {1:1,
+        2:1}
+
+def fibonacci(number):
+    if number <= 2:
+        return 1
+    elif number in seen:
+        return seen[number]
+    else:
+        seen[number] = fibonacci(number - 1) + fibonacci(number - 2)
+        return seen[number] 
+
+def find_fibonacci_index_by_length(num):
+    i = 2
+    curr = 0
+    while True:
+        curr = fibonacci(i)
+        if len(str(curr)) >= num:
+            return i
+        i += 1
+    return curr
+# All of these examples should print True
+# The first 12 fibonacci numbers are: 1 1 2 3 5 8 13 21 34 55 89 144
+print(find_fibonacci_index_by_length(2) == 7)
+print(find_fibonacci_index_by_length(3) == 12)
+print(find_fibonacci_index_by_length(10) == 45)
+print(find_fibonacci_index_by_length(16) == 74)
+print(find_fibonacci_index_by_length(100) == 476)
+print(find_fibonacci_index_by_length(1000) == 4782)
+
+# Next example might take a little while on older systems
+print(find_fibonacci_index_by_length(10000) == 47847)
